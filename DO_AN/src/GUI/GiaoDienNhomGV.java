a2b97a3898ca303fb3ca4ce1e4d70440e211102d
minkaze <phanhoangminh812@gmail.com>
2023-10-17 20:24:39 +0700

    GiaoDienNhomGV1,2

--- DO_AN/src/GUI/GiaoDienNhomGV.java
+++ DO_AN/src/GUI/GiaoDienNhomGV.java
@@ -1,16 +1,60 @@
 package GUI;
 
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class GiaoDienNhomGV extends JPanel {
-
-	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Create the panel.
 	 */
 	public GiaoDienNhomGV() {
-
+		setSize(1000, 600);
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(1000, 100));
+		add(panel, BorderLayout.NORTH);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0};
+		gbl_panel.rowWeights = new double[]{Double.MIN_VALUE, 0.0, 0.0, 0.0, 0.0};
+		panel.setLayout(gbl_panel);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1, BorderLayout.CENTER);
+        
+		JLabel lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon("D:\\DoAnCongNghePhanMem-main\\DO_AN\\img\\add.png"));
+        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+        gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel.insets = new Insets(30, 30, 30, 30);
+//        gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+        gbc_lblNewLabel.gridx = 0;
+        gbc_lblNewLabel.gridy = 3;
+        panel.add(lblNewLabel, gbc_lblNewLabel);
+        
+        
+        GridBagConstraints gbl = new GridBagConstraints();
+        // Khoảng cách 10px từ các viền
+        gbl.insets = new Insets(15, 15, 15, 15);
+        
+        gbl.gridx = 0; // Cột 0
+        gbl.gridy = 3; // Hàng 0
+        JLabel lb_monHoc = new JLabel("Các nhóm đang phụ trách");
+        lb_monHoc.setFont(new Font("Tahoma", Font.PLAIN, 30));
+        panel.add(lb_monHoc,gbl);
 	}
 
 }
--- DO_AN/src/GUI/GiaoDienNhomGV2.java
+++ DO_AN/src/GUI/GiaoDienNhomGV2.java
@@ -1,16 +1,68 @@
 package GUI;
 
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.Point;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import javax.swing.JSeparator;
 
 public class GiaoDienNhomGV2 extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
 
 	/**
 	 * Create the panel.
 	 */
 	public GiaoDienNhomGV2() {
+		setPreferredSize(new Dimension(1000, 600));
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setAlignmentY(Component.TOP_ALIGNMENT);
+		panel.setOpaque(false);
+		panel.setPreferredSize(new Dimension(250, 600));
+		add(panel, BorderLayout.CENTER);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		lblNewLabel = new JLabel("Mã nhóm");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel.setBorder(new EmptyBorder(50, 0, 5, 0));
+		panel.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("XYZ123");
+		lblNewLabel_1.setBorder(new EmptyBorder(10, 0, 10, 0));
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Môn học");
+		lblNewLabel_2.setBorder(new EmptyBorder(10, 0, 10, 0));
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("New label");
+		panel.add(lblNewLabel_3);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setPreferredSize(new Dimension(750, 600));
+		add(panel_1, BorderLayout.EAST);
+		
+		
 
 	}
-
 }
